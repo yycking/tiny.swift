66001190ca28e59d09d5c8568370176fb792d87c
YehYungCheng <yehyungcheng@YungChengtekiMacBook-Air.local>
2017-02-03 20:25:00 +0800

    Initial Commit

---
+++ tiny.swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tiny.swift
+//
+//  Created by YehYungCheng on 2017/2/3.
+//  Copyright © 2017年 YehYungCheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
